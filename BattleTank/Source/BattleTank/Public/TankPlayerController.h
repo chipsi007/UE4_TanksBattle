2c25bddcc5633da7b043348745a23a1a5aaf0605
Kamil <milek111@yahoo.com>
2017-10-31 08:09:47 +0000

    BT18 Finding screen pixel coorinates

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -28,4 +28,10 @@ private:
 	void AimTowrdsCrosshair();
 
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333;
+
 };
