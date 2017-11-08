6817b1de609bf5aa1ad33a9ee235b2cf8707999f
Kamil <milek111@yahoo.com>
2017-11-08 09:15:16 +0000

    BT29 Barrel Rotation Movement - using FMath::Clamp(x, min, max) for value clamping

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,6 +7,10 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the Barrel the right amount this frame
 	// Given a max elevation Speed, and the frame time
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
 	
+	SetRelativeRotation(FRotator(FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees), 0, 0));
 }
 
