884e82f84bc3e3188041f1083b479c01fd99c1a8
Kamil <milek111@yahoo.com>
2017-11-09 07:50:12 +0000

    BT31

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -63,10 +63,10 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
 	// Work-out difference between current barrel rotation, and AimDirection
-	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
+	auto BarrelRotation = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
-	auto DeltaRotator = AimAsRotator - BarrelRotator;
+	auto DeltaRotation = AimAsRotator - BarrelRotation;
 
-	Barrel->Elevate(DeltaRotator.Pitch);
+	Barrel->Elevate(5);
 	//Turret->Rotate(DeltaRotator.Yaw);
 }
