e7c50568dcde990680680a6964dbdaf7d58f0cc0
Kamil <milek111@yahoo.com>
2018-01-05 18:28:59 +0100

    BT59 - GREEN Coode - ready to refactor - !ensure issue resolved

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -18,6 +18,8 @@ ATank::ATank()
 void ATank::BeginPlay()
 {
 	Super::BeginPlay(); // Needed for BP Begin Play to run!
+
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
 }
 
 void ATank::AimAt(FVector HitLocation) 
